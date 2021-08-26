e20ad6f107aa7c6c56560569dc0d210d910e5b38
ggomma <ggomma@gmail.com>
2021-08-26 17:10:45 +0900

    Connect MainToken, SystemVariables contract to Factory

--- contracts/V0/Factory.sol
+++ contracts/V0/Factory.sol
@@ -2,14 +2,36 @@
 pragma solidity ^0.8.0;
 
 import {Avatar} from "./Avatar.sol";
+import {MainToken} from "./MainToken.sol";
+import {SystemVariables} from "./SystemVariables.sol";
 
 // TODO: 721 Compatible
 contract Factory {
+    SystemVariables public systemVariables;
+    MainToken public mainToken;
+    address public feePool;
+
     mapping(uint256 => address) ownerOf; // Avatar owner
     mapping(uint256 => address) avatarOf; // Avatar address
     uint256 public totalSupply = 0;
 
+    constructor(
+        SystemVariables _systemVariables,
+        MainToken _mainToken,
+        address _feePool
+    ) {
+        mainToken = _mainToken;
+        feePool = _feePool;
+        systemVariables = _systemVariables;
+    }
+
     function mint() public returns (address avatarAddress) {
+        uint256 amount = systemVariables.immigrantFee();
+
+        uint256 allowance = mainToken.allowance(msg.sender, address(this));
+        require(allowance >= amount, "Token allowance is not enough");
+        mainToken.transferFrom(msg.sender, feePool, amount);
+
         Avatar avatar = new Avatar(msg.sender);
         avatarAddress = address(avatar);
 
